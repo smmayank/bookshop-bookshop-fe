d6c59797caa8e19265cc39b18bcb9be90dc58d93
Mayank Saxena <sm.mayank@icloud.com>
2020-03-20 13:00:57 +0100

    removed console.log from post

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -3,12 +3,9 @@ import Title from '../../components/Title';
 import fetch from 'isomorphic-unfetch';
 
 const Post = (props) => {
-
     const { show } = props;
     const { name, summary, image } = show;
-
     const formattedSummary = summary;
-
     return (
         <Layout>
             {Title(name)}
@@ -22,11 +19,7 @@ Post.getInitialProps = async function (context) {
     const { id } = context.query;
     const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
     const show = await res.json();
-
-    console.log(`Fetched show: ${show}`);
-
     return { show };
 };
-
 
 export default Post;
